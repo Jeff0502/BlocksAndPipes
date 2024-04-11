02933866e65a380ac32c0c0089455e567f5c4ed0
Nelis <u24604611@tuks.co.za>
2024-04-11 14:32:34 +0200

    Updated Main to do window things

--- main.cpp
+++ main.cpp
@@ -6,12 +6,29 @@ int main(int argc, char *argv[])
 {
     std::cout << "Hello World";
 
-    if(SDL_Init(SDL_INIT_EVERYTHING) < 0)
-        std::cout << "Failed to initialize " << SDL_GetError();
-         
-    SDL_Window *window = SDL_CreateWindow("hello world", 0, 0, 800, 600, SDL_WINDOW_SHOWN);
+    if(SDL_Init(SDL_INIT_EVERYTHING) != 0)
+      {  
+        std::cout << "Failed to initialize " << SDL_GetError( ) << std::endl;
+        return 1;
 
-    while(true);
+      }
+         
+    SDL_Window *window = SDL_CreateWindow("hello world", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 800, 600, SDL_WINDOW_ALLOW_HIGHDPI);
+
+SDL_Event windowEvent;
+
+    while(true)
+    {
+        if (SDL_PollEvent( &windowEvent ))
+        {
+            if ( SDL_QUIT == windowEvent.type )
+            { break; }
+        }
+    }
+
+    SDL_DestroyWindow( window );
+    SDL_Quit( );
+
+    return EXIT_SUCCESS;
     
-    return 0;
 }
