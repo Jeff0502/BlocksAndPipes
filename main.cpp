7c7cde8513707a92a93fbfe380c9d61dd25068f0
Christiaan <jeffjefferson0502@gmail.com>
2024-04-11 19:33:52 +0200

    Drawed a simple Rectangle

--- main.cpp
+++ main.cpp
@@ -4,31 +4,53 @@
 
 int main(int argc, char *argv[])
 {
-    std::cout << "Hello World";
-
     if(SDL_Init(SDL_INIT_EVERYTHING) != 0)
-      {  
+    {  
         std::cout << "Failed to initialize " << SDL_GetError( ) << std::endl;
         return 1;
-
-      }
-         
+    }
+        
+    // Le box fancy box on your screen
     SDL_Window *window = SDL_CreateWindow("hello world", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 800, 600, SDL_WINDOW_ALLOW_HIGHDPI);
+    SDL_Event windowEvent;
 
-SDL_Event windowEvent;
+    // Le thing that actually renders things
+    SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, 0);
 
-    while(true)
+    // A rectangle to test everything (This thing is a struct, go goegel)
+    SDL_Rect rect = {
+      0, 0, 100, 100  
+    };
+
+  while(true)
+  {
+    // Clear the background
+    SDL_RenderClear(renderer);
+
+    // Colour for the rectangle
+    SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
+    SDL_RenderFillRect(renderer, &rect);
+
+    SDL_RenderPresent(renderer);
+
+    // Colour for the background, since RenderClear is called again
+    SDL_SetRenderDrawColor(renderer, 100, 149, 237, 255);
+
+    // Gets the current window event
+    if (SDL_PollEvent(&windowEvent))
     {
-        if (SDL_PollEvent( &windowEvent ))
-        {
-            if ( SDL_QUIT == windowEvent.type )
-            { break; }
+        if (SDL_QUIT == windowEvent.type)
+        { 
+            break; 
         }
     }
+  }
 
-    SDL_DestroyWindow( window );
-    SDL_Quit( );
+  // Frees up the space taken by the window and window
+  SDL_DestroyRenderer(renderer);
+  SDL_DestroyWindow(window);
+  SDL_Quit();
 
-    return EXIT_SUCCESS;
-    
+  return EXIT_SUCCESS;
+  
 }
